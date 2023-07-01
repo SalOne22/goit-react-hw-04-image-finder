f908ddb1d21667cd96bf9c59c382b1a76bf3e9ce
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-07-01 14:35:48 +0200

    Moved App.styled to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
-import { Container } from 'App.styled';
+import { Container } from './App.styled';
 import { Searchbar } from './Searchbar';
 import { ImageGallery } from './ImageGallery';
+import { Button } from './Button';
 
 export const App = () => {
   return (
     <Container>
       <Searchbar />
       <ImageGallery images={[]} />
+      <Button />
     </Container>
   );
 };
