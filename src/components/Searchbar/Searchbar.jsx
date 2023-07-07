c7495b6c94a529b5271e3780cdb6e3a2e60bb2e7
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-07-07 12:45:56 +0200

    Updated Searchbar to hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
   Header,
@@ -8,35 +7,33 @@ import {
   Input,
 } from './Searchbar.styled';
 
-export class Searchbar extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
-
-  handleSubmit = evt => {
+export const Searchbar = ({ onSubmit }) => {
+  const handleSubmit = evt => {
     evt.preventDefault();
     const value = evt.currentTarget.elements.query.value.trim();
 
-    this.props.onSubmit(value);
+    onSubmit(value);
   };
 
-  render() {
-    return (
-      <Header>
-        <SearchForm onSubmit={this.handleSubmit}>
-          <SubmitBtn type="submit">
-            <SubmitBtnLabel>Search</SubmitBtnLabel>
-          </SubmitBtn>
+  return (
+    <Header>
+      <SearchForm onSubmit={handleSubmit}>
+        <SubmitBtn type="submit">
+          <SubmitBtnLabel>Search</SubmitBtnLabel>
+        </SubmitBtn>
 
-          <Input
-            type="text"
-            name="query"
-            autocomplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </SearchForm>
-      </Header>
-    );
-  }
-}
+        <Input
+          type="text"
+          name="query"
+          autocomplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </SearchForm>
+    </Header>
+  );
+};
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
